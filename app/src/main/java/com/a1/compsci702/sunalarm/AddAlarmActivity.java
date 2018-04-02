dc92c9f3c3ca3a8ecb364442602fc4b0297d4258
st970703 <st970703@users.noreply.github.com>
2018-04-02 15:12:25 +1200

    Made the timepicker work in sunalarm\AddAlarmActivity.java

--- app/src/main/java/com/a1/compsci702/sunalarm/AddAlarmActivity.java
+++ app/src/main/java/com/a1/compsci702/sunalarm/AddAlarmActivity.java
@@ -3,8 +3,18 @@ package com.a1.compsci702.sunalarm;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
-public class AddAlarmActivity extends AppCompatActivity {
+import com.wdullaer.materialdatetimepicker.date.DatePickerDialog;
+import com.wdullaer.materialdatetimepicker.time.RadialPickerLayout;
+import com.wdullaer.materialdatetimepicker.time.TimePickerDialog;
+
+import java.util.Calendar;
+
+
+public class AddAlarmActivity extends AppCompatActivity implements
+        TimePickerDialog.OnTimeSetListener,
+        DatePickerDialog.OnDateSetListener {
 
     private final String TAG = "AddAlarmActivity";
 
@@ -13,5 +23,33 @@ public class AddAlarmActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_alarm);
         Log.d(TAG, "public class AddAlarmActivity");
+
+
+        this.setTime();
+    }
+
+    private void setTime(){
+        Log.d(TAG, "private void setTime(View v)");
+
+        Calendar now = Calendar.getInstance();
+        TimePickerDialog tpd = TimePickerDialog.newInstance(
+                AddAlarmActivity.this,
+                now.get(Calendar.HOUR_OF_DAY),
+                now.get(Calendar.MINUTE),
+                false
+        );
+        tpd.setThemeDark(false);
+        tpd.show(getFragmentManager(), "Timepickerdialog");
+    }
+
+    @Override
+    public void onDateSet(DatePickerDialog view, int year, int monthOfYear, int dayOfMonth) {
+    }
+
+    @Override
+    public void onTimeSet(TimePickerDialog view, int hourOfDay, int minute, int second) {
+        String time = "You picked the following time: "+hourOfDay+"h"+minute+"m";
+        Log.d(TAG, time);
+
     }
 }
