3e06f829161c9ec5ddbf9d9caa6937cedcb4732d
Yuan Wei Qi <yqi955@aucklanduni.ac.nz>
2018-04-02 12:22:35 +1200

    Implemented HTTP Get Request

--- app/src/main/java/com/a1/compsci702/sunalarm/HTTPRequest.java
+++ app/src/main/java/com/a1/compsci702/sunalarm/HTTPRequest.java
@@ -1,11 +1,55 @@
 package com.a1.compsci702.sunalarm;
 
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class HTTPRequest {
 
-    String _url;
+    private String _url;
+    private int _connectionTimeout_ms = 5000;
+    private int _readTimeout_ms = 5000;
 
-    public HTTPRequest(String url){
+    public HTTPRequest(String url) {
         _url = url;
+    }
+
+    public String makeGetRequest() throws MalformedURLException, IOException {
+
+        URL url = new URL(_url);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+
+        // connection.setRequestProperty("User-Agent", USER_AGENT);
+        connection.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+        connection.setUseCaches(false);
+        connection.setConnectTimeout(_connectionTimeout_ms);
+        connection.setReadTimeout(_readTimeout_ms);
+
+        int responseCode = connection.getResponseCode();
+        System.out.println("\nSending 'GET' request to URL : " + url);
+        System.out.println("Response Code : " + responseCode);
+
+        StringBuffer response = new StringBuffer();
+
+        if (responseCode == HttpURLConnection.HTTP_OK) {
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String inputLine;
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+        }
+
+        return response.toString();
     }
 
 }
