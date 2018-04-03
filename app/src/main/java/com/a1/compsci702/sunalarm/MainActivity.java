8aef25ee838a9ece8ec682c04b9f0f65e8dec3de
hbao448 <hbao448@aucklanduni.ac.nz>
2018-04-03 20:48:32 +1200

    line got deleted for some reason

--- app/src/main/java/com/a1/compsci702/sunalarm/MainActivity.java
+++ app/src/main/java/com/a1/compsci702/sunalarm/MainActivity.java
@@ -64,6 +64,7 @@ public class MainActivity extends AppCompatActivity {
         askForPermission();
         setUpUIComponents();
         loadAlarms();
+        cacheDates();
     }
 
     private void askForPermission() {
