a1eedc01e0f155ab74735eb5abd1000dd23d65e9
st970703 <st970703@users.noreply.github.com>
2018-04-07 17:22:33 +1200

    Added recurrence picker (days of the week)

--- app/src/main/java/com/a1/compsci702/sunalarm/AddAlarmActivity.java
+++ app/src/main/java/com/a1/compsci702/sunalarm/AddAlarmActivity.java
@@ -3,18 +3,28 @@ package com.a1.compsci702.sunalarm;
 import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.TextInputLayout;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.text.TextUtils;
+import android.text.format.Time;
 import android.view.View;
 import android.widget.Button;
 import android.widget.NumberPicker;
+import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
-public class AddAlarmActivity extends AppCompatActivity {
+import com.codetroopers.betterpickers.recurrencepicker.EventRecurrence;
+import com.codetroopers.betterpickers.recurrencepicker.EventRecurrenceFormatter;
+import com.codetroopers.betterpickers.recurrencepicker.RecurrencePickerDialogFragment;
+
+
+
+public class AddAlarmActivity extends AppCompatActivity implements RecurrencePickerDialogFragment.OnRecurrenceSetListener {
 
     private final String TAG = "AddAlarmActivity";
     private Button confirmButton;
@@ -24,6 +34,14 @@ public class AddAlarmActivity extends AppCompatActivity {
 
     private TextInputLayout _alarmNameWrapper;
 
+    private Button _repeatButton;
+
+    private static final String FRAG_TAG_RECUR_PICKER = "recurrencePickerDialogFragment";
+    private EventRecurrence mEventRecurrence = new EventRecurrence();
+    private String mRrule;
+
+
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,6 +50,7 @@ public class AddAlarmActivity extends AppCompatActivity {
 
         this.confirmButton = findViewById(R.id.confirm_button);
         this.offsetPicker = findViewById(R.id.offset_picker);
+        this._repeatButton = findViewById(R.id.repeat_button);
 
         this._alarmNameWrapper = findViewById(R.id.alarmNameWrapper);
 
@@ -74,9 +93,57 @@ public class AddAlarmActivity extends AppCompatActivity {
             }
         });
 
+        /*
+        From: https://github.com/code-troopers/android-betterpickers/blob/master/sample/src/main/java/com/codetroopers/betterpickers/sample/activity/recurrencepicker/SampleRecurrenceBasicUsage.java
+         */
+        _repeatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fm = getSupportFragmentManager();
+                Bundle bundle = new Bundle();
+                Time time = new Time();
+                time.setToNow();
+                bundle.putLong(RecurrencePickerDialogFragment.BUNDLE_START_TIME_MILLIS, time.toMillis(false));
+                bundle.putString(RecurrencePickerDialogFragment.BUNDLE_TIME_ZONE, time.timezone);
+
+                // may be more efficient to serialize and pass in EventRecurrence
+                bundle.putString(RecurrencePickerDialogFragment.BUNDLE_RRULE, mRrule);
+
+                RecurrencePickerDialogFragment rpd = (RecurrencePickerDialogFragment) fm.findFragmentByTag(
+                        FRAG_TAG_RECUR_PICKER);
+                if (rpd != null) {
+                    rpd.dismiss();
+                }
+                rpd = new RecurrencePickerDialogFragment();
+                rpd.setArguments(bundle);
+                rpd.setOnRecurrenceSetListener(AddAlarmActivity.this);
+                rpd.show(fm, FRAG_TAG_RECUR_PICKER);
+            }
+        });
+
+
+
         this._alarmTimePicker = findViewById(R.id.alarmTimePicker);
 
         _alarmTimePicker.setIs24HourView(true);
     }
 
+    @Override
+    public void onRecurrenceSet(String rrule) {
+        mRrule = rrule;
+        if (mRrule != null) {
+            mEventRecurrence.parse(mRrule);
+        }
+        populateRepeats();
+    }
+
+    private void populateRepeats() {
+        Resources r = getResources();
+        String repeatString = "";
+        if (!TextUtils.isEmpty(mRrule)) {
+            repeatString = EventRecurrenceFormatter.getRepeatString(this, r, mEventRecurrence, true);
+        }
+
+        Toast.makeText(this, mRrule + "\n" + repeatString, Toast.LENGTH_LONG).show();
+    }
 }
